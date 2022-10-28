89ab89e96d7ae438cdeda4c321427ae11d7831fa
LazyBone152 <>
2022-10-28 15:25:28 +1300

    Add missing operators to CustomMatRepUV.cs. Fixes duplicate materials being created when pasting.

--- LB_Common/Numbers/CustomMatRepUV.cs
+++ LB_Common/Numbers/CustomMatRepUV.cs
@@ -56,5 +56,36 @@ namespace LB_Common.Numbers
 
             return Values[idx];
         }
+
+        #region Operators
+        public override bool Equals(object obj)
+        {
+            return this == (CustomMatRepUV)obj;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash1 = 2256559;
+                const int hash2 = 5023567;
+                hash1 *= hash2 ^ U.GetHashCode();
+                hash1 *= hash2 ^ V.GetHashCode();
+                return hash1;
+            }
+        }
+
+        public static bool operator ==(CustomMatRepUV a, CustomMatRepUV b)
+        {
+            return a?.U == b?.U && a?.V == b?.V;
+        }
+
+        public static bool operator !=(CustomMatRepUV a, CustomMatRepUV b)
+        {
+            return (a == b) ? false : true;
+        }
+
+        #endregion
+
     }
 }
