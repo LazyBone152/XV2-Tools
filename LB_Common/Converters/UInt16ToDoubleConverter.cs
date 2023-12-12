874fbac0c7de5e9f9ae4a51cfbd273cd410ab5d2
LazyBone152 <>
2023-12-12 15:28:57 +1300

    UInt16ToDoubleConverter for WPF

---
+++ LB_Common/Converters/UInt16ToDoubleConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace LB_Common.Converters
+{
+    /// <summary>
+    /// Convert UShort into Double, properly accounting for the min and max value ranges.
+    /// </summary>
+    public class UInt16ToDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double?)
+            {
+                double val = (double)(double?)value;
+
+                if (val < 0) return ushort.MaxValue;
+                if(val > ushort.MaxValue) return ushort.MinValue;
+                return (ushort)val;
+            }
+            else
+            {
+                return Binding.DoNothing;
+            }
+        }
+    }
+}
