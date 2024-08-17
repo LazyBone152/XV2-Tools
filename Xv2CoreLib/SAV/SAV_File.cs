4c20e3b6be5ab5d402f80538158814c0a0f1be46
Pemdas <51948142+halloweenpandas@users.noreply.github.com>
2024-08-18 01:22:54 +0200

    add new save file version (#119)
    * add new save file version
    
    * Add tooltip for new sav ver
    
    ---------
    
    Co-authored-by: LazyBone152 <60512194+LazyBone152@users.noreply.github.com>

--- Xv2CoreLib/SAV/SAV_File.cs
+++ Xv2CoreLib/SAV/SAV_File.cs
@@ -669,6 +669,8 @@ namespace Xv2CoreLib.SAV
                         return "1.21.02";
                     case 34:
                         return "1.22.01";
+                    case 35:
+                        return "1.22.02";
                     default:
                         return String.Format("Unknown ({0})", Version);
 
@@ -718,6 +720,7 @@ namespace Xv2CoreLib.SAV
                     case 32:
                     case 33:
                     case 34:
+                    case 35:
                         return null;
                     default:
                         return "This save version is not supported. It is recommened to update the application (if one is available).";
@@ -764,6 +767,7 @@ namespace Xv2CoreLib.SAV
                     case 32:
                     case 33:
                     case 34:
+                    case 35:
                         return Brushes.Blue;
                     default:
                         return Brushes.Orange;
