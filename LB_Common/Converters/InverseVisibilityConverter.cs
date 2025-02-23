f966845b4c95b19dd3cf48dc9bce49262690f4b3
lazybone <>
2025-02-23 18:11:53 +1300

    EEPK Organiser: add arrow expander to effect list

---
+++ LB_Common/Converters/InverseVisibilityConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LB_Common.Converters
+{
+    public class InverseVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility visibility)
+            {
+                return InvertVisibiity(visibility);
+            }
+            else
+            {
+                return Binding.DoNothing;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility visibility)
+            {
+                return InvertVisibiity(visibility);
+            }
+            else
+            {
+                return Binding.DoNothing;
+            }
+        }
+    
+        private static Visibility InvertVisibiity(Visibility visibility)
+        {
+            return visibility == Visibility.Collapsed || visibility == Visibility.Hidden ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
